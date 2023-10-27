7c40e9656b69239d45293304d91bca4a10560aaf
unknown <thanhlongvn2002@gmail.com>
2023-10-27 09:49:47 +0700

    04-plus

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <Steps />
     </div>
   );
 }
@@ -42,40 +43,33 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
-
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              <span>👈</span>Previous
-            </Button>
-
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next<span>👉</span>
-            </Button>
+            <button
+              onClick={handlePrevious}
+              style={
+                step > 1
+                  ? { backgroundColor: "#7950f2", color: "#fff" }
+                  : { backgroundColor: "#fff", color: "#7950f2" }
+              }
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              style={
+                step < 1 || step > 2
+                  ? { backgroundColor: "#fff", color: "#7950f2" }
+                  : { backgroundColor: "#7950f2", color: "#fff" }
+              }
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <p className="message">
-      <h3> Step {step}</h3>
-      {children}
-    </p>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      onClick={onClick}
-      style={{ backgroundColor: bgColor, color: textColor }}
-    >
-      {children}
-    </button>
   );
 }
